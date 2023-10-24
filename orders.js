98d5fd97e56664b6d6363348f2ce389e931ec19f
mixedfeelings13 <fuegodjh@gmail.com>
2023-10-24 22:51:17 +0100

    first commit

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+  {
+    productName: "Jeans",
+    productNumber: 1,
+    paymentStatus: "Paid",
+    status: "Delivered",
+  },
+  {
+    productName: "Glasses",
+    productNumber: 1,
+    paymentStatus: "Unpaid",
+    status: "Declined",
+  },
+  {
+    productName: "Hat",
+    productNumber: 1,
+    paymentStatus: "Paid",
+    status: "Dispatched",
+  },
+]
